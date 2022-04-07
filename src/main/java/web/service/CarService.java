1913b475e1d3a4fe7ce7f150acf4762a62189cde
Merichka <nik-meri@yandex.ru>
2022-04-07 15:38:44 +0300

    Update CarService.java

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -7,13 +7,9 @@ import java.util.List;
 
 public class CarService {
     private List<Car> cars = new ArrayList<>();
-
-    public List<Car> getAllCars(){
-        cars.add(new Car("Tahoe Chevrolet", "White", 171));
-        cars.add(new Car("Porsche Cayenne", "Black", 777));
-        cars.add(new Car("Ferrari", "Red", 295));
-        cars.add(new Car("Lamborghini Diablo", "Metallic", 497));
-        cars.add(new Car("Aston Martin", "Blue", 538));
-        return  cars;
+    
+    public static List<Car> carsCount(List<Car> list, int number) {
+        if (number == 0 || number > 5) return list;
+        return list.stream().limit(number).collect(Collectors.toList());
     }
 }
