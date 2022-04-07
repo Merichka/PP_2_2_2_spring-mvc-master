d5df02d49f0ea737b54d7b162cbb339ab8631627
Merichka <nik-meri@yandex.ru>
2022-04-07 20:07:48 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -8,21 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
-public class CarsController {
-    @GetMapping("/cars")
-    public String cars(@RequestParam(value = "count", defaultValue = "5") int allCars, Model model) {
-        List<Car> list = new ArrayList<>();
-        list.add(new Car("Tahoe Chevrolet", "White", 171));
-        list.add(new Car("Porsche Cayenne", "Black", 777));
-        list.add(new Car("Ferrari", "Red", 295));
-        list.add(new Car("Lamborghini Diablo", "Metallic", 497));
-        list.add(new Car("Aston Martin", "Blue", 538));
-        list = CarService.carsCount(list, allCars);
-        model.addAttribute("list", list);
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public String carList(@RequestParam(required = false, name = "count", defaultValue = "5") int count,
+                          ModelMap model) {
+        if (count < 1) {
+            model.addAttribute("cars", null);
+        } else {
+            model.addAttribute("cars", carService.getCars(count));
+        }
         return "cars";
     }
 }
-
 
 
 
