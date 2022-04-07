a5b78c29987e50892f37d6978f25f8183b21c464
Merichka <nik-meri@yandex.ru>
2022-04-07 19:58:47 +0300

    Update CarService.java

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,15 +1,23 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import web.model.Car;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class CarService {
-    private List<Car> cars = new ArrayList<>();
-    
-    public static List<Car> carsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
-        return list.stream().limit(number).collect(Collectors.toList());
+    private List<Car> cars = new ArrayList<Car>() {{
+        add(new Car("Tahoe Chevrolet", "White", 171));
+        add(new Car("Porsche Cayenne", "Black", 777));
+        add(new Car("Ferrari", "Red", 295));
+        add(new Car("Lamborghini Diablo", "Metallic", 497));
+        add(new Car("Aston Martin", "Blue", 538));
+    }};
+
+    public List<Car> getCars(int number) {
+        return cars.stream().limit(number).toList();
     }
 }
